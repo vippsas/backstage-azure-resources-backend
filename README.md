8f8717d7391bab7d3ec5502db5913446e54e7903
Martin Ehrnst <martin.ehrnst@vipps.no>
2022-08-30 14:37:16 +0200

    first

---
+++ README.md
@@ -0,0 +1,48 @@
+# Azure resources backend plugin
+
+The backend API plugin used by the frontend plugin. This plugin wraps a few Azure Resource Graph queries to pull information about your Azure resource resources in relation to a backstage entity.
+
+
+## Getting started
+
+Add following to your backstage instance
+
+```TypeScript
+/// ./packages/backend/src/azure-resources-backend.ts
+import { createRouter } from '@internal/plugin-azure-resources-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
+  return await createRouter({
+    logger: env.logger
+  });
+}
+```
+
+```TypeScript
+/// ./packages/backend/src/index.ts
+
+import azureResourcesBackend from './plugins/azure-resources-backend';
+const azureResourcesBackendEnv = useHotMemoize(module, () => createEnv('azure-resources-backend'));
+apiRouter.use('/azure-resources', await azureResourcesBackend(azureResourcesBackendEnv));
+```
+
+## Authentication with Azure
+
+In order for this plugin to work, you'll need an identity with `read` access at the scope you pull information from.  
+Depending on where your backstage instance is running, there are a few ways to authenticate. The plugin uses `@azure/identity` which supports `managed identities` and `application registration`
+
+### Using managed identity
+
+In case your backstage instance runs on an Azure resource type that supports system assignen managed identity, the only thing you need to do is assign the `service principal` with read permission on the `resource group`, `subscription(s)` or `management group`, depending on your environment.
+
+### App registration
+
+Create a [new Application Registration](https://docs.microsoft.com/azure/active-directory/develop/quickstart-register-app?WT.mc_id=AZ-MVP-5003437), and assign the SPN appropriate permissions. Then add the following `environment variables` to your application.
+
+- `AZURE_CLIENT_ID`
+- `AZURE_CLIENT_SECRET`
+- `AZURE_TENANT_ID`
---
+++ src/service/router.ts
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { errorHandler } from '@backstage/backend-common';
+import express from 'express';
+import Router from 'express-promise-router';
+import { Logger } from 'winston';
+import { Config } from '@backstage/config';
+import { ResourceGraphClient } from '@azure/arm-resourcegraph';
+import { DefaultAzureCredential} from '@azure/identity';
+
+export interface RouterOptions {
+  logger: Logger;
+  config: Config;
+}
+const auth = new DefaultAzureCredential();
+const client = new ResourceGraphClient(auth);
+
+export async function createRouter(
+  options: RouterOptions,
+): Promise<express.Router> {
+  const { logger } = options;
+
+  const router = Router();
+  router.use(express.json());
+
+  router.get('/health', (_, response) => {
+    logger.info('PONG!');
+    response.send({ status: 'OK - azure resource backend api' });
+  });
+
+  router.get('/rg/:tagKey/:tagValue', (req, response) => {
+    const tagKey = req.params.tagKey
+    const tagValue = req.params.tagValue
+    if (!tagKey) {
+      return response.send({
+        error: 'name must be defined'
+      });
+    }
+    const query = `ResourceContainers
+| where type =~ "microsoft.resources/subscriptions/resourcegroups"
+| where tags["${tagKey}"] =~ "${tagValue}"`;
+    logger.info(query);
+    return client.resources(
+      { query: query },
+      { resultFormat: 'table' }
+    ).then(result => {
+      response.send({
+        total: result.count,
+        data: result.data
+      });
+    }).catch(e => {
+      response.status(500).send({
+        error: e
+      });
+    });
+  });
+
+  router.get('/rg/:tagKey/:tagValue/secrecommendations', (req, response) => {
+    const client = new ResourceGraphClient(auth);
+    const tagKey = req.params.tagKey
+    const tagValue = req.params.tagValue
+
+    if (!tagKey) {
+      return response.send({
+        error: 'name must be defined'
+      });
+    }
+    const query = `ResourceContainers
+    | where type =~ "microsoft.resources/subscriptions/resourcegroups"
+    | where tags["${tagKey}"] =~ "${tagValue}"
+    | join (securityresources
+        | where type == 'microsoft.security/assessments'
+        | extend statusCode=properties.status.code,
+            resourceId=tolower(properties.resourceDetails.Id),
+            severity = properties.metadata.severity,
+            displayName = properties.metadata.displayName,
+            link = properties.links.azurePortal
+        | join kind=leftouter(
+            resources | extend resourceName = name
+                    | extend resourceId = tolower(id)
+                    | extend resourceType = type
+                    | project resourceName, resourceId, resourceType) on resourceId
+    ) on resourceGroup
+| where statusCode =~"Unhealthy"
+| project resourceId, displayName, link, resourceName, resourceType, resourceGroup, severity`;
+    logger.info(query);
+    return client.resources(
+        { query: query },
+        { resultFormat: 'table' }
+    ).then(result => {
+      response.send({
+        total: result.count,
+        data: result.data
+      });
+    }).catch(e => {
+      response.status(500).send({
+        error: e
+      });
+    });
+  });
+
+  router.use(errorHandler());
+  return router;
+}
